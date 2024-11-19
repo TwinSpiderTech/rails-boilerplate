b503548c493d3064302b86823015a3cfc830ba7e
Mutafaf Wahhaj <mwhjnsr@gmail.com>
2024-11-19 23:59:48 +0500

    Added author module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_022833) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_19_164108) do
+  create_table "authors", force: :cascade do |t|
+    t.string "author_name"
+    t.string "phone"
+    t.string "address"
+    t.integer "published_books"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+
 User.create!(email: 'me@admin.com', password: 'password')
+
+authors = 30.times.map do
+  Author.create!(
+    author_name: Faker::Name.name,
+    phone: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.full_address,
+    published_books: rand(10..50)
+  )
+end
